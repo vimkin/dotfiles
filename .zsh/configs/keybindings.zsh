6ada44f46b196b0764ae514166e7ba81ed41eba7
Vadim Kalinin <vimkin.github@gmail.com>
2021-05-07 01:10:45 +0200

    refactor: remove unused keybindings

--- .zsh/configs/keybindings.zsh
+++ .zsh/configs/keybindings.zsh
@@ -1,11 +1,6 @@
-# give us access to ^Q
-stty -ixon
-
 # vi mode
 bindkey -v
 bindkey "^F" vi-cmd-mode
-
-# handy keybindings
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 bindkey "^K" kill-line
@@ -13,5 +8,3 @@ bindkey "^R" history-incremental-search-backward
 bindkey "^P" history-search-backward
 bindkey "^Y" accept-and-hold
 bindkey "^N" insert-last-word
-bindkey "^Q" push-line-or-edit
-bindkey -s "^T" "^[Isudo ^[A" # "t" for "toughguy"
